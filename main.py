a01be2b563866a5b718c442504e546c8d7ecb01e
unknown <daniel.kamali@yahoo.com>
2019-09-01 11:08:10 +0430

    first_Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+class Game:
+    cards = []
+
+
+class card:
+    
+
+
+
+
+
+
+
+
+
+
+from telegram.ext import Updater
+updater = Updater(token='747856178:AAG55_tg9z-7HK8nCkN5g1B1Eah9s0fmzuo', use_context=True)
+
+
+
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a butt, please talk to me!")
+
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
