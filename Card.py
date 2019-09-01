3855e7c900ff132889a58901f13b042f8b30c7f7
mohammad <mohamad.sanaye462@zoho.com>
2019-09-01 13:37:04 +0430

    ADD constructor for game and card

--- Card.py
+++ Card.py
@@ -1,14 +1,13 @@
 class card:
     name = ""
     picture = ""
-    action = ""
-    def __init__(self, *args, **kwargs):
-
-        return super().__init__(*args, **kwargs)
-
+    action = [] 
+    def __init__(self, card_name, card_action):
+        name = card_name
+        action = card_action
     def get_action(self):
 
         return self.action
 
     def is_eligable_action(self,action):
-        return self.get_action() == action
+        return self.get_action() ==action
--- Game.py
+++ Game.py
@@ -1,2 +1,18 @@
+import player
 class Game:
+    turn = 0
     cards = []
+    players = []
+    state = "" #Challenging, Over, Acting, Revealing, Starting
+
+    def _init_(self,player_list):
+        state = "Starting"
+        turn = player_list[0]
+        for p in player_list:
+            players.append(Player(p))
+        for i in range(3):
+            cards.append(Card('Duke', ['TAX','FOA']))
+            cards.append(Card('Captain', ['STE','BLK'])) #Block steal
+            cards.append(Card('Ambassador', ['EXC','BLK'])) #Block steal
+            cards.append(Card('Assassin', ['ASI']))
+            cards.append(Card('Contessa', ['BLO'])) #Block assassinationi
